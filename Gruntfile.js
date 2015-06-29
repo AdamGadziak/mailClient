e6143cb692da8824490bde1ffae7c7a7db945e14
Adam Gądziak <adamgadziak@gmail.com>
2015-06-29 02:37:42 +0200

    simple structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,15 +3,17 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    // jshint: {
-    //   files: ['src/**/*.js', 'test/**/*.spec.js'],
-    //   options: {
-    //     // options here to override JSHint defaults
-    //     jshintrc: true
-    //   }
-    // }
+    jshint: {
+      files: ['src/**/*.js', 'test/**/*.spec.js'],
+      options: {
+        // options here to override JSHint defaults
+        jshintrc: true
+      }
+    }
   });
 
   // Load the plugin that provides the "jshint" task.
-  // grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.registerTask('check', ['jshint:files']);
+  grunt.registerTask('default', ['check']);
 };
---
+++ src/app.js
@@ -0,0 +1,39 @@
+app = angular.module('myapp', ['ui.router']).config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/");
+    $stateProvider
+	.state('inbox', {
+		url: "/inbox",
+		templateUrl: "src/views/nbox.html",
+		controller: 'InboxCtrl'
+	})
+	.state('home', {
+		url: "/",
+		templateUrl: "src/views/inbox.html",
+		controller: 'InboxCtrl'
+	})
+	.state('email', {
+		url: "/view/:emailId",
+		templateUrl: "src/views/view.html",
+		controller: 'viewCtrl'
+	})
+	.state('sent', {
+		url: "/sent",
+		templateUrl: "src/views/sent.html",
+		controller: 'SentCtrl'
+	})
+	.state('create', {
+		url: "/create",
+		templateUrl: "src/views/create.html",
+		controller: 'CreateCtrl'
+	})
+	.state('reply', {
+		url: "/create/:emailId",
+		templateUrl: "src/views/create.html",
+		controller: 'CreateCtrl'
+	})
+	.state('config', {
+		url: "/config",
+		templateUrl: "src/views/config.html",
+		controller: 'ConfigCtrl'
+	});
+});
