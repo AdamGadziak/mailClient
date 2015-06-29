bf69af5a7f97b51d8633e59ff88b0d835c1a5b52
Adam Gądziak <adamgadziak@gmail.com>
2015-06-29 03:10:53 +0200

    fix service

---
+++ src/service.js
@@ -0,0 +1,61 @@
+app.service('emailService', function($http, $window, $interval) {
+    
+    var localStorage = $window.localStorage;
+    var defaultInterval = 10;
+    var intervalAction;
+
+    return {
+        clearIntervalAction: function() {
+            $interval.cancel(intervalAction);
+        },
+
+        setIntervalAction: function(action) {
+            intervalAction = action;
+        },
+
+        getEmails: function() {
+            return $http.get('/emails').then(function(res) {
+                return res;
+            });
+        },
+
+        getSent: function() {
+            return $http.get('/sent').then(function(res) {
+                return res;
+            });
+        },
+
+        deleteEmail: function(id) {
+            return $http.delete('/emails/' + id).then(function(res) {
+                return res;
+            });
+        },
+
+        sendEmail: function(email) {
+            email.id = Math.floor((Math.random() * 10000000) + 10000000);
+            email.sent = new Date().getTime();
+
+            return $http.post('/sent', email).then(function(res) {
+                return res;
+            });
+        },
+
+        setAsRead: function(id) {
+            return $http.put('/emails/' + id, {
+                read: true
+            }).then(function(res) {
+                return res;
+            });
+        },
+
+        getRefreshInterval: function() {
+            return parseInt(localStorage.interval) || defaultInterval;
+        },
+
+        getEmail: function(id) {
+            return $http.get('/emails/' + id).then(function(res) {
+                return res;
+            });
+        }
+    };
+});
