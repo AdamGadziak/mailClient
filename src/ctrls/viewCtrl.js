51aa6fe7f7b07ec7accba1de148dc7dc5e5a07f0
Adam Gądziak <adamgadziak@gmail.com>
2015-06-29 03:35:15 +0200

    single mail viewing ctrl

---
+++ src/ctrls/viewCtrl.js
@@ -0,0 +1,24 @@
+app.controller('EmailCtrl', function($scope, $http, $location, service) {
+    var email_id = $location.path().split(":")[1];
+    $scope.email = {};
+    $scope.getEmail = function() {
+        service.getEmail(email_id).then(function(email){
+            $scope.email = {
+                id: email.data.id,
+                title: email.data.title,
+                date: email.data.sent || email.data.received,
+                sender: email.data.sender || "Ja",
+                receiver: email.data.receiver || "Ja",
+                content: email.data.content
+            };
+        });
+    };
+    $scope.callToDeleteEmail = function() {
+        service.deleteEmail(email_id).then(function(res){
+            $location.path("/inbox");
+        });
+    };
+    $scope.respond = function(){
+    	$location.path( "/create/:"+$scope.email.id);
+    };
+});
