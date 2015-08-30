f8dba867dff8344e377948df9c0c0179e8090e92
Adam Gądziak <adamgadziak@gmail.com>
2015-08-31 01:59:59 +0200

    highlighting search word

--- src/directives.js
+++ src/directives.js
@@ -11,3 +11,11 @@ app.directive('menu', function () {
         controller: 'ConfigCtrl'
     };
 });
+app.filter('highlight', function($sce) {
+    return function(text, phrase) {
+        if (phrase) {
+        	text = text.replace(new RegExp('('+phrase+')', 'gi'),'<span class="highlighted">$1</span>');
+        }
+        return $sce.trustAsHtml(text);
+    };
+});
